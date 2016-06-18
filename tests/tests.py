9f0a1fc7c0b494677bb0042db896284d5269a0d6
Jim Haughwout <jim.haughwout@gmail.com>
2016-06-18 16:37:18 -0400

    Added unittests to ensure target URLs return 200

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 import theword as tw 
 
 class TheWordTest(unittest.TestCase):
@@ -9,11 +10,13 @@ class TheWordTest(unittest.TestCase):
 		"""
 		self.assertIsInstance(tw.TheWord(), tw.TheBird)
 
+
 	def test_assignment(self):
 		"""
 		Tests assignment
 		"""
 		self.assertEquals(1964, tw.TheBird().released)
+
 
 	def test_question(self):
 		"""
@@ -21,3 +24,19 @@ class TheWordTest(unittest.TestCase):
 		"""
 		self.assertEquals("Bird, bird, bird, b-bird's the word", 
 			tw.what_is_the_word().message)
+
+
+	def test_song_link(self):
+		"""
+		Tests if _The Trashmen_ link is active
+		"""
+		request = requests.get('https://www.youtube.com/watch?v=aPrtFxd9u9Y')
+		self.assertEquals(200, request.status_code)
+
+
+	def test_parody_link(self):
+		"""
+		Tests if _The Family Guy_ link is active
+		"""
+		request = requests.get('https://www.youtube.com/watch?v=2WNrx2jq184')
+		self.assertEquals(200, request.status_code)
